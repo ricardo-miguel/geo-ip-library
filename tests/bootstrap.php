471849bf7b9005e2f88a8549509a2625d8237628
Ricardo Miguel <yo@ricardomiguel.cl>
2017-10-15 04:58:22 -0300

    bootstrap CI hotfix #2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/geo-ip-library.php';
+	require dirname( dirname( __FILE__ ) ) . '/src/geo-ip-library.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
